f3e275db31ac8cd40e0cc558fea77031bbd5bf16
jvphung <79540999+jvphung@users.noreply.github.com>
2021-02-23 13:06:24 -0700

    Add name and edited addNumber() and reverseNumber()
    -Added name to Line 2.
    -addNumber() now adds to previous number.
    -added a new reverseNum variable and have reverseNumber() return that instead of newNumber.

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+ * Jason Phung
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum;
     
     public GitDemo() {
         previousNumber = 1;
@@ -14,7 +15,7 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
-        return 0;
+        return num + previousNumber;
     }
     
     /*****
@@ -46,7 +47,9 @@ public class GitDemo {
             newNumber = newNumber * i;
         }
         
-        return newNumber;
+        reverseNum = newNumber;
+        
+        return reverseNum;
     }
     
 }
